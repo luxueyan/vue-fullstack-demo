a316ee22f73d459f5335ceb8335e3f14c210e042
luxueyan <luxueyan@kaitongamc.com>
2017-02-09 18:10:45 +0800

    commit dist files for heroku

---
+++ test/unit/specs/Hello.spec.js
@@ -0,0 +1,16 @@
+// import Vue from 'vue'
+// import Hello from 'src/components/Hello'
+
+describe('Hello.vue', () => {
+  it('should render correct contents', () => {
+    let a = '1'
+    expect(a).to.equal('1')
+      // Vue.url('src', { haha: 1 }).to.qual('src?haha=1')
+      // const vm = new Vue({
+      //   el: document.createElement('div'),
+      //   render: (h) => h(Hello)
+      // })
+      // expect(vm.$el.querySelector('.hello h1').textContent)
+      //   .to.equal('Welcome to Your Vue.js App')
+  })
+})
